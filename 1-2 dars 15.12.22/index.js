69095ebc86d568fabb1a24338a59f7ae1058ed81
Ramazon <generallg5390@gmail.com>
2022-12-24 14:59:18 -0800

    Variables,Data Types, Operators , TypeConversion Comparison

---
+++ 1-2 dars 15.12.22/index.js
@@ -0,0 +1,113 @@
+// let a = BigInt(48);
+// console.log(a);
+// let b = 1;
+// let c = "2";
+// console.log(b + c);
+//======= Data conversion
+// let Arr2 = [1, 2, 3];
+// let obj1 = { name: "acer" };
+// // console.log(Arr2 + obj1);
+// // console.log(isNaN(c));
+// // let v = "ferran";
+// // console.log(Number(d));
+// console.log(false == 0);
+// console.log(false == undefined);
+// console.log(undefined == null);
+// console.log(false == null);
+// console.log("please your data down raws!!!");
+// console.log(true == 1);
+// console.log(true == 0);
+// console.log(true == "1");
+// console.log(true == "01");
+// console.log(true == "a");
+// let a = 5,
+//     b = 118;
+// console.log(b % a);
+
+// let n = 35231;
+
+// function digitize(n) {
+//     let number = (num) => Number(num);
+//     let newArrays = Array.from(String(n), number);
+//     let newArray = newArrays.reverse();
+//     console.log(newArrays.length);
+
+//     return newArray;
+// }
+// digitize(n);
+// let myFunc2 = (num) => Number(num);
+// let newArrays = Array.from(String(n), myFunc2);
+// console.log(newArrays);
+// var myInt = 235345; // Getting the string as a parameter // and typecasting it
+
+// let myFunc = (num) => Number(num);
+// var intArr = Array.from(String(myInt), myFunc); // Print the result array
+// console.log(intArr);
+
+// console.log(digitize(n));
+
+// let str1 = "world";
+
+// function word(str) {
+//     let newstring = "";
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         // let newstring = "";
+//         newstring += str[i];
+//     }
+//     return newstring;
+// }
+// word(str1);
+// console.log(word(str1));
+
+// let f = 2;
+// console.log(f++);
+
+// console.log(f++);
+// let a = 11,
+//     c = 15;
+// let b = 11;
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a <= b);
+// console.log(c <= a);
+// console.log(a == b);
+// console.log(a === b);
+// console.log(typeof a);
+// console.log(typeof b);
+// let a = 48,
+//     b = "string",
+//     check = false;
+// console.log(a === b);
+// console.log(!a === !b);
+// console.log(!b);
+// console.log(!check);
+// let num = (number) => Number(num);
+// let Xstring = String.fromCharCode(b);
+// console.log(Xstring);
+
+// Logical comparisopns;
+// let ceo = false,
+//     deo = "keo",
+//     leo = "neo";
+// beo = false;
+// console.log(ceo || beo);
+// console.log(beo | ceo);
+// console.log(beo);
+// console.log(deo === "string" || beo);
+// console.log(deo === "string");
+// console.log(leo || deo); //javob deoning qiymatiga teng
+// console.log(deo || leo); //javob leo ning qiymatiga teng.
+
+// // And &&
+// let session = true,
+//     id = false,
+//     name = "name",
+//     raws = true;
+// console.log(session && false);
+
+//homework
+let name = "name";
+// alert(`hello ${1}`);
+alert(`hello ${name}`);
+alert(`hello ${"name"}`);
+alert(`hello ${single}`);
