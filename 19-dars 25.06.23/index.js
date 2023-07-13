7f8fd9e3af260872a292c752269c92d24cc8c1ca
unknown <generallg5390@gmail.com>
2023-07-13 05:28:29 -0700

    Decorator Function(Cache),bind,call,apply

--- 19-dars 25.06.23/index.js
+++ 19-dars 25.06.23/index.js
@@ -34,7 +34,7 @@ let users = [
   },
 ];
 function getFull() {
-  console.log(`${this.name} ${this.lastname}`);
+  console.log(`${this.name} ${this.lastname}`, getFull());
 }
 // user2.getFull();//bunda user2 uchun function ishlamaydi
 // getFull.call(user); //bunda functionning native metodlari ya'ni call ni chaqiramiz
@@ -48,26 +48,39 @@ function getFull() {
 // }
 
 //call ishlatilish maqsadi functionning dynamik yozilishi uchun osonlashgan yo'ldir
-
+//callga this orqali murojat qilish orqali contextni nomidan foydalanmay turib undan foydalanish mnumkin
+function k(a) {
+  return console.log(a);
+}
+a = 2;
+k.call(this, a); //=k(a) ga teng nazariy olib qaralsa call ga brinchi context sifatida  berilgan qiymat this bo'lsa
+//this shu functionnimng o'ziga osiladi va shuni qaytaradi
 //apply() - call ga o'xshaydi ammo argumentlarda farq qiladi
 // getFull.apply(user);
 
 //callda parametrlar berish
-function getHalf(a, b) {
-  console.log(`${this.name}`, a, b);
-}
+// function getHalf(a, b) {
+//   console.log(`${this.name}`, a, b);
+// }
 // getHalf.call(user, "Dilijon", "Umbrella");
 
 //callda funjction parametrlariga qiymat berib keta olamiz ammo agar parametrlar sonidan ortsa udefined beradi va function parametrlari 2 parametrdan foydalanib beradi
 
 //applyda  parametrlar uchun array typedan foydalanamiz parametrlar calldan farqli arrayning o indexidan boshlab hisoblanadi va array tashqarisida verguldan keyingi parametrlar qo'llanmaydi
 // getHalf.apply(this, ["Fortepiano", "Bethoven"]);
-//thisArg ga thi yozsak unda this yana undefioned bo'lib qoladi
+//thisArg ga this yozsak unda this yana undefioned bo'lib qoladi
 
 //bind() - function bo'lib ishlaydi boshqa functionning copysini beradi result qaytarmaydi
-getFull.bind(user); //result bermaydi
-getFull.bind(user)(); //natija berish uchun invoke qilish
-let m = getFull.bind(user); //alohida funksiyaga olindi
-m(); //alohida qilib chaqirilganda result beradi va ma'lum qiymatga ega
-let x = m();
-console.log(x); //undefined ma'lum qiymatga ega emas
+// getFull.bind(user); //result bermaydi
+// getFull.bind(user)(); //natija berish uchun invoke qilish
+// let m = getFull.bind(user); //alohida funksiyaga olindi
+// m(); //alohida qilib chaqirilganda result beradi va ma'lum qiymatga ega
+// let x = m();
+// console.log(x); //undefined ma'lum qiymatga ega emas
+
+for (let key in user) {
+  if (typeof user[key] == "function") {
+    user[key] = user[key].bind(user);
+  }
+}
+console.log(user.getFull());
